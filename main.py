b78bf3330d10dba11bdeab5a056fb444a0a506a8
carmancg <107522558+carmancg@users.noreply.github.com>
2025-05-02 22:05:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import random
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # O puedes poner ["https://tu-usuario.github.io"] para más seguridad
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class PalabrasInput(BaseModel):
     palabras: list[str]
 
